33020e87d2c0d6005588ddb4cbcfbe7343bd7e16
v_D <nguyenducviet280301@gmail.com>
2024-08-18 00:24:06 +0700

    Boy or girl

---
+++ boy_girl.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ()
+{
+    char str[101];
+    int i;
+    scanf("%s", str);
+    if( str[i] % 2 == 0){       // số ký tự là chẵn thì là con gái
+        printf("CHAT WITH HER!");
+    }else
+    {
+        printf("IGNORE HIM!");
+    }
+
+    return 0;
+}
